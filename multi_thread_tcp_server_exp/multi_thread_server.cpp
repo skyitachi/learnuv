9c79148adc705ead8f1e45cd817a3d6a23e0586a
skyitachi <skbysp@gmail.com>
2019-06-18 18:02:55 +0800

    fix: use raw fd

--- multi_thread_tcp_server_exp/multi_thread_server.cpp
+++ multi_thread_tcp_server_exp/multi_thread_server.cpp
@@ -2,29 +2,40 @@
 // Created by skyitachi on 2019-06-18.
 //
 // 一个失败的尝试, 只能从uv_accept中进行线程创建？？？
+// round 2: 使用raw fd in other thread
 
 #include <uv.h>
 #include <thread>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <iostream>
 
 void on_new_connection(uv_stream_t* server, int status) {
   if (status < 0) {
     fprintf(stdout, "listen error %s\n", uv_strerror(status));
     return;
   }
-  std::thread t1([server]() {
-    uv_loop_t *loop = uv_loop_new();
-    uv_tcp_t* client = (uv_tcp_t* )malloc(sizeof(uv_tcp_t));
-    uv_tcp_init(loop, client);
-    uv_run(loop, UV_RUN_ONCE);
-    printf("before in another thread accept\n");
-    int r = uv_accept(server, (uv_stream_t* )client);
-    if (r < 0) {
-      fprintf(stdout, "accept error %s\n", uv_strerror(r));
-      return;
+  uv_loop_t* loop = uv_default_loop();
+  uv_tcp_t* client = (uv_tcp_t* )malloc(sizeof(uv_tcp_t));
+  uv_tcp_init(loop, client);
+  int r = uv_accept(server, (uv_stream_t* )client);
+  if (r < 0) {
+    fprintf(stdout, "accept error %s\n", uv_strerror(r));
+    return;
+  }
+  int fd = client->accepted_fd;
+  std::thread t1([fd](){
+    char buf[1024];
+    int size = ::read(fd, buf, sizeof(buf));
+    if (size >= 0) {
+      std::cout << "receive data " << std::string(buf, size) << std::endl;
+    } else {
+      std::cerr << "read error happens " << strerror(errno) << std::endl;
     }
-    printf("accepted in another thread ok\n");
   });
   t1.join();
+  
 }
 
 int main() {

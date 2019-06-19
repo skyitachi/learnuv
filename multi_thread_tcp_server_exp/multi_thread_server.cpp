d0be6fba248da770a74a5c0ed0afb04940b54238
skyitachi <skbysp@gmail.com>
2019-06-19 09:14:31 +0800

    成功在child thread使用了新的loop

--- multi_thread_tcp_server_exp/multi_thread_server.cpp
+++ multi_thread_tcp_server_exp/multi_thread_server.cpp
@@ -11,6 +11,8 @@
 #include <sys/uio.h>
 #include <iostream>
 
+char globalBuf[8192];
+
 void on_new_connection(uv_stream_t* server, int status) {
   if (status < 0) {
     fprintf(stdout, "listen error %s\n", uv_strerror(status));
@@ -24,15 +26,45 @@ void on_new_connection(uv_stream_t* server, int status) {
     fprintf(stdout, "accept error %s\n", uv_strerror(r));
     return;
   }
-  int fd = client->accepted_fd;
+  uv_os_fd_t os_fd;
+  uv_fileno((const uv_handle_t* )client, &os_fd);
+  
+  int fd = os_fd;
+  printf("fd is %d\n", fd);
   std::thread t1([fd](){
-    char buf[1024];
-    int size = ::read(fd, buf, sizeof(buf));
-    if (size >= 0) {
-      std::cout << "receive data " << std::string(buf, size) << std::endl;
-    } else {
-      std::cerr << "read error happens " << strerror(errno) << std::endl;
-    }
+//    char buf[1024];
+//    int size = ::read(fd, buf, sizeof(buf));
+//    if (size >= 0) {
+//      std::cout << "receive data " << std::string(buf, size) << std::endl;
+//    } else {
+//      std::cerr << "read error happens " << strerror(errno) << std::endl;
+//    }
+    uv_tcp_t* child = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+    uv_loop_t* loop1 = uv_loop_new();
+    uv_tcp_init(loop1, child);
+    uv_tcp_open(child, fd);
+    uv_read_start(
+      (uv_stream_t *)child,
+      [](uv_handle_t *handle, size_t suggest_size, uv_buf_t* buf){
+        *buf = uv_buf_init(globalBuf, sizeof(globalBuf));
+      },
+      [](uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
+        if (nread < 0) {
+          fprintf(stdout, "child thread read error %s\n", uv_strerror(nread));
+          return;
+        } else {
+          fprintf(stdout, "receive data %s\n", buf->base);
+          uv_write_t* req = (uv_write_t*)malloc(sizeof(uv_write_t));
+          uv_write(req, stream, buf, 1, [](uv_write_t* req, int status) {
+            free(req);
+            if (status < 0) {
+              fprintf(stderr, "child write error %s\n", uv_strerror(status));
+              return;
+            }
+          });
+        }
+      });
+    uv_run(loop1, UV_RUN_DEFAULT);
   });
   t1.join();
   
